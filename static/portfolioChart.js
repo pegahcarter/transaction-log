da2527fb033d4f73e9ab40f4963eff5b7a7c294b
Carter Carlson <cartercarlson94@gmail.com>
2018-11-07 08:11:27 -0600

    Fixed the graph!  Now there's just some minor adjustments

--- static/portfolioChart.js
+++ static/portfolioChart.js
@@ -20,23 +20,14 @@ for (var i=0; i < graphLabels.length; i++) {
 
 
 var margin = {
-	top: 0,
-	right: 0,
-	bottom: 0,
+	top: 10,
+	right: 50,
+	bottom: 20,
 	left: 50
 }
 
-width = 250 - margin.left - margin.right
+width = 600 - margin.left - margin.right
 height = 200 - margin.top - margin.bottom
-
-
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3.select("#portfolioGraph").append("svg")
-	.attr("height", width + margin.left + margin.right)
-	.attr("width", height + margin.top + margin.bottom)
-
-var chartGroup = svg.append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var x = d3.scale.linear()
 	.range([0, width])
@@ -44,12 +35,12 @@ var x = d3.scale.linear()
 
 var y = d3.scale.ordinal()
 	.domain(data.map(d => d.name))
-	.rangeRoundBands([0, height])
-	.padding(0.1);
+	.rangeRoundBands([0, height], .2)
 
 var xAxis = d3.svg.axis()
 	.scale(x)
-	.orient("bottom");
+	.orient("bottom")
+	.tickFormat(d3.format(".0%"));
 
 var yAxis = d3.svg.axis()
 	.scale(y)
@@ -57,21 +48,39 @@ var yAxis = d3.svg.axis()
 	.tickSize(0)
 	.tickPadding(6);
 
-chartGroup.append("g")
-	.call(xAxis);
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("#portfolioGraph").append("svg")
+		.attr("height", width + margin.left + margin.right)
+		.attr("width", height + margin.top + margin.bottom)
 
-chartGroup.append("g")
-	.call(yAxis)
-	.attr("transform", "translate(0," + width + ")");
+var chartGroup = svg.append("g")
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+x.domain(d3.extent(data, d => d.value)).nice();
+y.domain(data.map(d => d.name));
 
 chartGroup.selectAll(".bar")
 		.data(data)
 	.enter().append("rect")
-		.attr("class", "bar")
+		.attr("class", d => "bar bar--" + (d.value < 0 ? "negative" : "positive"))
 		.attr("x", d => x(Math.min(0, d.value)))
 		.attr("y", d => y(d.name))
 		.attr("width", d => Math.abs(x(d.value) - x(0)))
-		.attr("height", y.rangeBand());
+		.attr("height", y.rangeBand()-2)
 
+chartGroup.append("g")
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + height + ")")
+	.call(xAxis);
+
+chartGroup.append("g")
+	.attr("class", "y axis")
+	.attr("transform", "translate(" + x(0) + ",0)")
+	.call(yAxis);
+
+d3.select('svg')
+	.attr("width", "600px")
+	.attr("height", "200px")
+	.style("border-style", "ridge")
 
 // https://bl.ocks.org/mbostock/2368837
