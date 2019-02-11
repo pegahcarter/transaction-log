92dbb15a15ded94ca61509dceeb12c8389d503f0
Carter Carlson <cartercarlson94@gmail.com>
2019-02-11 16:37:22 -0600

    Add simulation to main class.  Still need to test

--- py/models.py
+++ py/models.py
@@ -3,6 +3,7 @@ import pandas as pd
 import exchange
 import ccxt
 
+# TODO: add column into simulations CSV for coin price at time of trade
 class Portfolio(object):
 	'''
 	Represents our account balance on Binance
@@ -11,22 +12,28 @@ class Portfolio(object):
 	prices 	- list of the most recent dollar price for each coin held
 	d_vals  - list of the dollar values for each coin held (units * prices)
 	'''
-	def __init__(self):
-		df = pd.read_csv('../api.csv')
-		binance = ccxt.binance({'options': {'adjustForTimeDifference': True},
-								'apiKey': df['apiKey'],
-								'secret': df['secret']})
+	def __init__(self, coins=None, d_amt=None):
+		if coins:
+			hist_prices = pd.read_csv('../data/historical/prices.csv')
+			prices = [hist_prices[coin][0] for coin in coins]
+			units =  np.divide(prices, d_amt/len(coins))
+		else:
+			df = pd.read_csv('../api.csv')
+			binance = ccxt.binance({'options': {'adjustForTimeDifference': True},
+									'apiKey': df['apiKey'],
+									'secret': df['secret']})
 
-		balance = binance.fetchBalance()
-		# Only rebalance the coins we hold with at least 0.01 units (accounts for coin dust)
-		coins =	[asset['asset']
-				 for asset in balance['info']['balances']
-				 if (float(asset['free']) > 0.01)]
+			balance = binance.fetchBalance()
+			# Only rebalance the coins we hold with at least 0.01 units (accounts for coin dust)
+			coins =	[asset['asset']
+					 for asset in balance['info']['balances']
+					 if (float(asset['free']) > 0.01)]
 
-		units = np.array([balance[coin]['total'] for coin in coins])
-		prices = [exchange.fetch_price(coin) for coin in coins]
+			units = np.array([balance[coin]['total'] for coin in coins])
+			prices = [exchange.fetch_price(coin) for coin in coins]
+			self.binance = binance
+
 		self.coins = coins
 		self.units = units
 		self.prices = prices
 		self.d_vals = units * prices
-		self.binance = binance
